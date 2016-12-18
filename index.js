d1a683ea1c353ba9da15d0a50e79701d108c5239
jjusianiec <jax1994@gmail.com>
2016-12-18 12:55:06 +0100

    try this

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
+// var apiProxy = httpProxy.createProxyServer();
 var frontend = 'testjiragoggles-fronted.herokuapp.com';
 var backend = 'testjiragoggles-backend.herokuapp.com';
 var reverse = 'jiragoggles.herokuapp.com';
@@ -12,41 +12,48 @@ var logger = function(req, res, next) {
     next(); // Passing the request to the next handler in the stack.
 };
 
-app.use(logger);
-
-
-app.all("/", function (req, res) {
+var server = httpProxy.createServer(function (req, res, proxy) {
     req.headers.host = backend;
-    apiProxy.web(req, res, {target: backend});
-});
+    proxy.proxyRequest(req, res, {
+        host: backend
+    });
+}).listen(process.env.PORT);
 
-app.all("/api/*", function(req, res) {
-    console.log('redirecting to backend');
-    req.headers.host = backend;
-    apiProxy.web(req, res, {target: backend});
-});
-
-app.all("/atlassian-connect.json", function (req, res) {
-    console.log("redirect to backend");
-    req.headers.host = backend;
-    apiProxy.web(req, res, {target: backend});
-});
-
-app.all("/installed", function(req, res) {
-    console.log('redirecting to backend');
-    apiProxy.web(req, res, {target: backend});
-});
-
-app.all("/main", function(req, res) {
-    console.log('redirecting to backend');
-    apiProxy.web(req, res, {target: backend});
-});
-
-app.all("/*", function(req, res) {
-    console.log('redirecting to fronted');
-    apiProxy.web(req, res, {target: frontend});
-});
-
-
-console.log("Reverse proxy is up and running");
-app.listen(process.env.PORT);
+// app.use(logger);
+//
+//
+// app.all("/", function (req, res) {
+//     req.headers.host = backend;
+//     apiProxy.web(req, res, {target: backend});
+// });
+//
+// app.all("/api/*", function(req, res) {
+//     console.log('redirecting to backend');
+//     req.headers.host = backend;
+//     apiProxy.web(req, res, {target: backend});
+// });
+//
+// app.all("/atlassian-connect.json", function (req, res) {
+//     console.log("redirect to backend");
+//     req.headers.host = backend;
+//     apiProxy.web(req, res, {target: backend});
+// });
+//
+// app.all("/installed", function(req, res) {
+//     console.log('redirecting to backend');
+//     apiProxy.web(req, res, {target: backend});
+// });
+//
+// app.all("/main", function(req, res) {
+//     console.log('redirecting to backend');
+//     apiProxy.web(req, res, {target: backend});
+// });
+//
+// app.all("/*", function(req, res) {
+//     console.log('redirecting to fronted');
+//     apiProxy.web(req, res, {target: frontend});
+// });
+//
+//
+// console.log("Reverse proxy is up and running");
+// app.listen(process.env.PORT);
