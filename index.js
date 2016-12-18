9b191a3b756ea251ca2b95ce6f1a367e4b9ad48c
jjusianiec <jax1994@gmail.com>
2016-12-18 04:57:01 +0100

    testing host

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.all("/api/*", function(req, res) {
 app.all("/atlassian-connect.json", function (req, res) {
     console.log("redirect to backend");
     console.log(req);
+    req.headers.host = backend;
     apiProxy.web(req, res, {target: backend});
 });
 
